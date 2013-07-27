2fec9f5e438981048ef9bef560443494dd36e752
reborg <reborg@reborg.net>
2013-07-27 09:38:15 +0100

    General projct.clj updates.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject pwc "0.0.1-SNAPSHOT"
-  :description "Cool new project to do things and stuff"
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :description "The venerable wc unix command, parallel version."
+  :dependencies [[org.clojure/clojure "1.5.1"]]
   :plugins [[lein-midje "3.1.1"]]
   :profiles {:dev {:dependencies [[midje "1.5.1"]]}})
