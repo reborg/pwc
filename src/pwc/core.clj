0790ba55ea5d33e30dcf057774d41e0b174ec755
Renzo Borgatti <reborg@reborg.net>
2013-09-17 12:16:51 +0100

    a new flag just to test jvm warmup time

--- src/pwc/core.clj
+++ src/pwc/core.clj
@@ -6,6 +6,7 @@
 
 (def options [["-f" "--frequencies" "output frequencies of words" :flag true]
               ["-c" "--chars" "not implemented" :flag true]
+              ["-t" "--test" "empty main just to measure jvm warmup" :flag true]
               ["-l" "--lines" "not implemented" :flag true]
               ["-m" "--multibyte" "not implemented" :flag true]
               ["-w" "--words" "not implemented" :flag true]])
@@ -21,6 +22,8 @@
 (defn -main [& args]
   "pwc execution main entry point"
   (let [[opts args banner] (apply cli args options)]
+    (if (:test opts)
+      (print-msg-and-exit "Empty execution to mesure JVM startup time"))
     (if (empty-args? args)
       (print-msg-and-exit (str "Missing input file: pwc [-clmw] <file>. Other flags" banner))
       (let [rs (wf (iota/seq (first args)) (:frequencies opts))
